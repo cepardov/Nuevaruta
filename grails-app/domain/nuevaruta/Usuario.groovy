4818876e3ae5dd9693191813eedc127add56cbf6
Pablo Santana <pablo.santana.r@gmail.com>
2016-11-17 10:24:07 -0300

    Cambios usuario Domain y agrega boostrap init

--- grails-app/domain/nuevaruta/Usuario.groovy
+++ grails-app/domain/nuevaruta/Usuario.groovy
@@ -2,9 +2,11 @@ package nuevaruta
 
 class Usuario {
     String rut, nombres, paterno, materno, telefono, correo, tipo
-    Date fechaCreacion, fehcaNacimiento
+    Date fechaCreacion, fechaNacimiento
     Integer estado
     static hasMany = [sucursal:Sucursal]
     static constraints = {
+        fechaCreacion nullable: true, blank:true
+        fechaNacimiento nullable: true, blank:true
     }
 }
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,30 @@
+import nuevaruta.Sucursal
+import nuevaruta.Usuario
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = {
+        if (!Usuario.count()){
+            new Usuario(rut: "18.279.378-3",
+                    nombres: "Pablo Andrés",
+                    paterno: "Zumelzu",
+                    materno: "Zumelzu",
+                    correo: "pablo@zumelzu.cl",
+                    telefono: "1234456768"
+            ).save(failOnError:true)
+        }
+        if (!Sucursal.count()){
+            new Sucursal(rut: "76.816.854-3",
+                    nombre: "nuevaRuta Puerto Montt",
+                    direccion: "Puerto Montt, #376, alerce sur",
+                    logo: "/img/logo.png",
+                    region: "Puerto Montt",
+                    ciudad: "Puerto Montt",
+                    fono: "123456689",
+                    usuario: "1"
+            ).save(failOnError:true)
+        }
+
     }
     def destroy = {
     }
